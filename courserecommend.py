027aab233850a388db38edcfd02e4e90ec8a61a0
RPeshy44 <RPesce22@gmail.com>
2023-05-09 00:29:53 -0400

    added proper functionality to showElectives(), changed getInput() to exclude duplicate entries in the returned list

--- courserecommend.py
+++ courserecommend.py
@@ -17,6 +17,7 @@ def getInput():
     print("Please enter the course code for each computer science (CS) and mathematics (MA) course you will have finished before next semester.")
     print("Enter each course using the same format (capital letters, no spaces) as the following example: 'CS101'")
     print("Press the enter/return key after entering each individual course.")
+    print("Please note that any input that is irrelevant or formatted incorrectly will not be recognized and will therefore not affect the list of courses for which you are eligible.")
     print("Type 'DONE' in all caps and press the enter/return key after you have entered every course.")
     print("")
 
@@ -29,43 +30,58 @@ def getInput():
         if userInput != "DONE":
             userList.append(userInput)
 
-    # Prints the list of the user's entered courses.
+    # Removes all duplicate entries and prints the list of the user's entered courses.
+    userList = [*set(userList)]
     print("")
-    print("YOUR COMPLETED COURSES: ")
+    print("YOU ENTERED THE FOLLOWING AS YOUR COMPLETED COURSES: ")
     print(userList)
     print("")
     print("")
 
     return userList
 
-def showElectives():
-    # Initializes variables: electivesList contains all of the Elective objects for the possible elective courses
-    electivesList = electives.allElectives()
+def showElectives(userList):
+    # Initializes variables: electivesList contains all of the Elective objects for the possible elective courses, eligibleElectives will contain the Elective objects of the classes for which the user is eligible
+    allElectives = electives.allElectives()
+    eligibleElectives = []
+
+    # Fills the list eleigibleELectives with the courses for which the user is eligible. 
+    for electiveCourse in allElectives:
+        if electiveCourse.requirement == []:
+            eligibleElectives.append(electiveCourse)
+        else:
+            requirementsSatisfied = 0
+            for requiredCourse in electiveCourse.requirement:
+                for completedCourse in userList:
+                    if completedCourse == requiredCourse:
+                        requirementsSatisfied += 1
+                if requirementsSatisfied == len(electiveCourse.requirement):
+                    eligibleElectives.append(electiveCourse)
 
 
     print("YOU ARE ELIGIBLE FOR THE FOLLOWING ELECTIVES: ")
     print("")
 
-    # IN ITS CURRENT STATE, THIS PRINTS ALL THE POSSIBLE ELECTIVE COURSES, REGARDLESS OF THE USER'S INPUT.
+    # Prints all courses for which the user is eligible that fall under the category of "Software Development Electives"
     print("Software Development Electives: ")
-    for elective in electivesList:
+    for elective in eligibleElectives:
         if elective.sd:
             print(elective.id + " - " + elective.name)
 
     print("")
 
+    # Prints all courses for which the user is eligible that fall under the category of "Technical Electives"
     print("Technical Electives: ")
-    for elective in electivesList:
+    for elective in eligibleElectives:
         if elective.tech:
-            print(elective.id + " - " + elective.name)
+             print(elective.id + " - " + elective.name)
 
     print("")
     print("Please note that any courses that appear in both categories (Software Development Electives and Technical Electives) can only be used as credit for one category or the other, not both.")
     print("")
 
-    return electivesList
+    return eligibleElectives
 
 if __name__ == '__main__':
-    # Calling the two functions
-    getInput()
-    showElectives()
+    # Main function
+    showElectives(getInput())
